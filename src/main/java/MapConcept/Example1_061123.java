94038c2623bd56035582c20f5c8228bed2c52f59
krunal45 <krunal45@outlook.com>
2023-11-06 13:10:25 +0530

    Map concept practice 06 Nov 2023

---
+++ src/main/java/MapConcept/Example1_061123.java
@@ -0,0 +1,22 @@
+package MapConcept;
+
+import java.util.*;
+
+public class Example1_061123 {
+
+    static Map<String, Integer> wordFrequencyCounter(List<String> words) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            word = word.toLowerCase().replaceAll("[\\!\\\"\\'\\,\\.\\;\\:\\?\\(\\)\\[\\]\\{\\}]", "");
+            if (map.containsKey(word))
+                map.put(word, map.get(word) + 1);
+            else
+                map.put(word, 1);
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(wordFrequencyCounter(new ArrayList<>(Arrays.asList("Tom","Dick","Herry","Tom","Dick","Ajay","Ajay.","ajay"))));
+    }
+}
