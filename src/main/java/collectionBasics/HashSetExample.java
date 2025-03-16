8444e719506851dd14af1dc73bdd2b2fc2b25aa5
qaprayogshala <qaprayogshala@gmail.com>
2025-03-16 17:28:56 +0530

    Code to demo Hash set Methods Example.

---
+++ src/main/java/collectionBasics/HashSetExample.java
@@ -0,0 +1,16 @@
+package collectionBasics;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<Character> alphabets = new HashSet<>();
+        System.out.println("Default Capacity: "+alphabets.size());
+        alphabets.add('X');
+        alphabets.add('B');
+        alphabets.add('Y');
+        alphabets.add('D');
+        alphabets.add(null);
+        System.out.println(alphabets);
+    }
+}
