eb53a092bf1924d592fbf8ad94c5c09d2d827d6b
qaprayogshala <qaprayogshala@gmail.com>
2025-03-28 13:26:37 +0530

    Using Comparator to order Tree Map Elements.

---
+++ src/main/java/TreeMapExample.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class TreeMapExample {
+
+    public static void main(String[] args) {
+        Comparator<String> lastNameDescendingOrder = (o1, o2) -> {
+            String lastName1 = o1.substring(o1.lastIndexOf(" ") + 1);
+            String lastName2 = o2.substring(o2.lastIndexOf(" ") + 1);
+            return lastName2.compareTo(lastName1);
+        };
+
+//        Comparator<String> lastNameAscendingOrder = (o1, o2) -> {
+//            String lastName1 = o1.substring(o1.lastIndexOf(" ") + 1);
+//            String lastName2 = o2.substring(o2.lastIndexOf(" ") + 1);
+//            return lastName2.compareTo(lastName1);
+//        };
+
+        Map<String, Integer> names = new TreeMap<>(lastNameDescendingOrder);
+        names.put("Alok Bhatt", 1000);
+        names.put("Bob Cheung", 2000);
+        names.put("Charu Upadhyay", 2500);
+        names.put("David Agarval", 3000);
+        System.out.println(names);
+    }
+}
