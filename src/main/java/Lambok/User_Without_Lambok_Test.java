db72321b0c432fae5fea77ddb091585515de6b80
qaprayogshala <qaprayogshala@gmail.com>
2025-05-16 16:18:02 +0530

    User Without Lambok Test

---
+++ src/main/java/Lambok/User_Without_Lambok_Test.java
@@ -0,0 +1,17 @@
+package Lambok;
+
+public class User_Without_Lambok_Test {
+    public static void main(String[] args) {
+        User_Without_Lambok user1 = new User_Without_Lambok("Test1", "001", "Male");
+//        Printing user Details:
+        System.out.println(user1);
+//        Updating User Details:
+        user1.setGender("Female");
+        user1.setId("002");
+        user1.setName("Test2");
+//        Fetching User Details:
+        System.out.println("Name : "+user1.getName());
+        System.out.println("Id : "+user1.getId());
+        System.out.println("Gender : "+user1.getGender());
+    }
+}
