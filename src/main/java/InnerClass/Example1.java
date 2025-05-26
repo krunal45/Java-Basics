df448d91cbb0e52ff1a4b76f127d1209d04412e6
qaprayogshala <qaprayogshala@gmail.com>
2025-05-26 14:43:40 +0530

    Inner Class Example

---
+++ src/main/java/InnerClass/Example1.java
@@ -0,0 +1,24 @@
+package InnerClass;
+
+class A{
+    int num = 5;
+
+    void show(){
+        System.out.println("--Num--"+num);
+    }
+
+    class B{
+        void showB(){
+            System.out.println("--In B--");
+        }
+    }
+}
+
+public class Example1 {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+        A.B obj1 = obj.new B();
+        obj1.showB();
+    }
+}
