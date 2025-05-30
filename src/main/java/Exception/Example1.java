3714899ed76cd9092d19f28db6ba313ee5dd968e
qaprayogshala <qaprayogshala@gmail.com>
2025-05-30 14:23:56 +0530

    Try-Catch Exception Code Example

---
+++ src/main/java/Exception/Example1.java
@@ -0,0 +1,36 @@
+//        **Question:**
+//Write a Java program that takes two integers as input from the user and performs division.
+// Use a `try-catch` block to handle the following exceptions:
+//
+//        1. **`ArithmeticException`** – when the user tries to divide by zero.
+//        2. **`InputMismatchException`** – when the user enters a non-integer value.
+//
+//Print appropriate error messages for each exception and ensure the program does not crash.
+
+package Exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Example1 {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = 0,j = 0,result = 0;
+        try{
+            System.out.println("Please Enter 2 numbers");
+            i = input.nextInt();
+            j = input.nextInt();
+            result = i / j;
+            System.out.println(result);
+        }catch (InputMismatchException e){
+            System.out.println("Invalid Input!");
+        }
+        catch (ArithmeticException e){
+            System.out.println("Cannot divide a number by 0!");
+        }finally {
+            input.close();
+        }
+    }
+}
