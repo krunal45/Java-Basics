318c86c070f9fbf2dfcf6a870ae8fa89b4a977e9
Krunal_Bhatt <krunal.jayeshkumarbhatt@nbrown.co.uk>
2025-11-15 06:45:29 +0530

    Code Practice

--- src/main/arrayBasics/ArraysClassExample.java
+++ src/main/arrayBasics/ArraysClassExample.java
@@ -1,38 +1,39 @@
-package main.arrayBasics;
-
-import java.util.Arrays;
-
-public class ArraysClassExample {
-    public static void main(String[] args) {
-//        Integer[] numbers = {1,2,4};
-//        List<Integer> list = Arrays.asList(numbers);
-////        list.add(5); > This won't be allowed.
-//        System.out.println(list);
-
-        Character[] chars = {'D','A','B','C'};
-        Character[] charsMatch = {'D','A','B','C'};
-        Character[] charsNotMatch = {'D','A','B','C','E'};
-        Arrays.sort(chars);
-        Arrays.sort(charsMatch);
-        Arrays.sort(charsNotMatch);
-        System.out.println(Arrays.toString(chars));
-//        Search for character 'A'
-        int index = Arrays.binarySearch(chars, 'E');
-        System.out.println("index of character A: "+index);
-//        Comparing two arrays
-        int val = Arrays.compare(chars, charsNotMatch);
-
-        if (val == 0)
-            System.out.println("Arrays are equal.");
-        else
-            System.out.println("Arrays are not equal.");
-
-//        fill()
-//        version 1
-        Arrays.fill(charsNotMatch, 'A');
-        System.out.println(Arrays.toString(charsNotMatch));
-
-// ...existing code...
-    }
-}
-
+//package main.arrayBasics;
+//
+//import java.util.Arrays;
+//import java.util.Optional;
+//
+//public class ArraysClassExample {
+//    public static void main(String[] args) {
+////        Integer[] numbers = {1,2,4};
+////        List<Integer> list = Arrays.asList(numbers);
+//////        list.add(5); > This won't be allowed.
+////        System.out.println(list);
+//
+//        Character[] chars = {'D','A','B','C'};
+//        Character[] charsMatch = {'D','A','B','C'};
+//        Character[] charsNotMatch = {'D','A','B','C','E'};
+//        Arrays.sort(chars);
+//        Arrays.sort(charsMatch);
+//        Arrays.sort(charsNotMatch);
+//        System.out.println(Arrays.toString(chars));
+////        Search for character 'A'
+//        int index = Arrays.binarySearch(chars, Optional.of('E'));
+//        System.out.println("index of character A: "+index);
+////        Comparing two arrays
+//        int val = Arrays.compare(chars, charsNotMatch);
+//
+//        if (val == 0)
+//            System.out.println("Arrays are equal.");
+//        else
+//            System.out.println("Arrays are not equal.");
+//
+////        fill()
+////        version 1
+//        Arrays.fill(charsNotMatch, 'A');
+//        System.out.println(Arrays.toString(charsNotMatch));
+//
+//// ...existing code...
+//    }
+//}
+//
